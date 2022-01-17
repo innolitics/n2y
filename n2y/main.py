fb0cd485b72511ccede14470221533bf075d92ae
Ilya Spivakov <xaker00@gmail.com>
2022-01-17 09:35:25 -0800

    output to markdown files

--- n2y/main.py
+++ n2y/main.py
@@ -1,10 +1,12 @@
 import os
 import sys
 import argparse
+import re
 
 import yaml
+import pandoc
 
-from n2y import notion, simplify
+from n2y import notion, simplify, converter
 
 
 def main():
@@ -15,20 +17,31 @@ def main():
 
     parser = argparse.ArgumentParser(description="Move data from Notion into YAML files")
     parser.add_argument("database", help="The Notion database id or share url")
-    parser.add_argument(
-        "--raw", action='store_true',
-        help="Dump the raw notion API data",
-    )
+    # Is this still needed?
+    #
+    # parser.add_argument(
+    #     "--raw", action='store_true',
+    #     help="Dump the raw notion API data",
+    # )
     args = parser.parse_args()
     database_id = notion.id_from_share_link(args.database)
     client = notion.Client(ACCESS_TOKEN)
     raw_rows = client.get_database(database_id)
-    if args.raw:
-        print(yaml.dump(raw_rows))
-        return 0
 
-    simplified_rows = simplify.flatten_database_rows(raw_rows)
-    print(yaml.dump(simplified_rows))
+    for row in raw_rows:
+        meta = simplify.flatten_database_row(row)
+        print(f"Processing {meta['title']}")
+        markdown = pandoc.write(
+            converter.convert({'content': client.get_page(row['id'])}), format='gfm')\
+            .replace('\r\n', '\n')  # Deal with Windows line endings
+        # sanitize file name just a bit
+        # maybe use python-slugify in the future?
+        filename = re.sub(r"[/,\\]", '_', meta['title'])
+        with open(f"{filename}.md", 'w') as f:
+            f.write('---\n')
+            f.write(yaml.dump(meta))
+            f.write('---\n\n')
+            f.write(markdown)
     return 0
 
 
