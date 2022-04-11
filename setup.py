d0d98ecaf9dae811c6b026b2ea10035961a67ff6
Sam'an Herman-Griffiths <shermangriffiths@innolitics.com>
2022-04-11 13:52:50 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ description = 'Notion to YAML'
 
 setup(
     name='n2y',
-    version='0.2.1',
+    version='0.2.3',
     description=description,
     long_description=description,
     long_description_content_type='text/x-rst',
