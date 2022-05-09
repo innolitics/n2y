4938efe41ec4bc4608f46b90f87f617ac2f0fc6a
J. David Giese <johndgiese@gmail.com>
2022-05-08 23:12:29 -0400

    Add custom exception classes with error codes
    This code was taken from the n2y python API library, partly to make it
    easier to switch to using that library in the future if we think it's
    worthwhile. It's helpful to have the error status codes on the
    exceptions so you can properly react to the errors.

---
+++ tests/utils.py
@@ -0,0 +1,3 @@
+# This is not a security risk because the integration token that provides
+# read-only access to public notion pages in a throw-away notion account.
+NOTION_ACCESS_TOKEN = 'secret_lylx4iL5awveY3re6opuvSQqM6sMRu572TowhfzPy5r'
