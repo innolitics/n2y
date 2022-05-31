6798a68e7cbc5e5bc3888c48b65c6e897b0e9586
J. David Giese <johndgiese@gmail.com>
2022-05-31 17:39:56 -0400

    Speed optimization

--- n2y/utils.py
+++ n2y/utils.py
@@ -1,14 +1,33 @@
 from datetime import datetime
+import logging
 
 import pandoc
+from pandoc.types import Str, Space
+
+
+logger = logging.getLogger(__name__)
 
 
 def pandoc_ast_to_markdown(pandoc_ast):
-    return pandoc.write(
-        pandoc_ast,
-        format='gfm+tex_math_dollars',
-        options=['--wrap', 'none'],  # don't hard line-wrap
-    ).replace('\r\n', '\n')
+    # This function tries to avoid calling the separate pandoc binary (which is
+    # slow) for basic cases with just
+    if len(pandoc_ast) == 0:
+        return ""
+    elif all(type(n) in [Str, Space] for n in pandoc_ast):
+        # TODO: optimize performance for some other basic cases
+        return ''.join(
+            ' ' if isinstance(n, Space) else n[0]
+            for n in pandoc_ast
+        )
+    else:
+        result = pandoc.write(
+            pandoc_ast,
+            format='gfm+tex_math_dollars',
+            options=['--wrap', 'none'],  # don't hard line-wrap
+        ).replace('\r\n', '\n')
+        logger.debug("%s", result)
+        logger.debug("%s", repr(pandoc_ast))
+        return result
 
 
 def fromisoformat(datestring):
