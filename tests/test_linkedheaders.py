639fc58d7563ed0ad3616207fa672ca766c22244
Bimba Shrestha <bshrestha.msae@gmail.com>
2022-08-15 09:05:08 -0400

    Clean up markdown asset with notion_block['url']

--- tests/test_linkedheaders.py
+++ tests/test_linkedheaders.py
@@ -29,25 +29,25 @@ def test_heading_1():
     notion_block = mock_block("heading_1", {"rich_text": [mock_rich_text("Heading One")]})
     pandoc_ast, markdown = process_block(notion_block, ["n2y.plugins.linkedheaders"])
     assert pandoc_ast == mock_header_ast(1, "One", notion_block)
-    assert markdown == f'# [Heading One](#{strip_hyphens(notion_block["id"])})\n'
+    assert markdown == f'# [Heading One]({notion_block["url"]})\n'
 
 
 def test_heading_1_bolding_stripped():
     notion_block = mock_block("heading_1", {"rich_text": [mock_rich_text("Heading One", ["bold"])]})
     pandoc_ast, markdown = process_block(notion_block, ["n2y.plugins.linkedheaders"])
     assert pandoc_ast == mock_header_ast(1, "One", notion_block)
-    assert markdown == f'# [Heading One](#{strip_hyphens(notion_block["id"])})\n'
+    assert markdown == f'# [Heading One]({notion_block["url"]})\n'
 
 
 def test_heading_2():
     notion_block = mock_block("heading_2", {"rich_text": [mock_rich_text("Heading Two")]})
     pandoc_ast, markdown = process_block(notion_block, ["n2y.plugins.linkedheaders"])
     assert pandoc_ast == mock_header_ast(2, "Two", notion_block)
-    assert markdown == f'## [Heading Two](#{strip_hyphens(notion_block["id"])})\n'
+    assert markdown == f'## [Heading Two]({notion_block["url"]})\n'
 
 
 def test_heading_3():
     notion_block = mock_block("heading_3", {"rich_text": [mock_rich_text("Heading Three")]})
     pandoc_ast, markdown = process_block(notion_block, ["n2y.plugins.linkedheaders"])
     assert pandoc_ast == mock_header_ast(3, "Three", notion_block)
-    assert markdown == f'### [Heading Three](#{strip_hyphens(notion_block["id"])})\n'
+    assert markdown == f'### [Heading Three]({notion_block["url"]})\n'
