c75d2d142971e5fe5b0e9a332c274348452b2093
J. David Giese <johndgiese@gmail.com>
2022-09-13 14:50:55 -0500

    Update version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ description = 'Notion to YAML'
 
 setup(
     name='n2y',
-    version='0.4.2',
+    version='0.5.0',
     description=description,
     long_description=description,
     long_description_content_type='text/x-rst',
