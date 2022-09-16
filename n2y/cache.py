da577772a1aa44743d4936475a49cad33159dc8d
Alex Mountjoy <alex.mountjoy@gmail.com>
2022-09-15 23:05:12 -0700

    Sketches of new modules

---
+++ n2y/cache.py
@@ -0,0 +1,16 @@
+import logging
+
+
+logger = logging.getLogger(__name__)
+
+class Cache:
+
+    def __init__(self, client):
+        self.client = client
+
+    def add_to_cache():
+        None
+
+
+    def get_from_cache():
+        None
