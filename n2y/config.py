76d6cb091e9789c5f6f8b1bb0b411664266fe00e
J. David Giese <johndgiese@gmail.com>
2022-09-28 19:11:49 -0400

    Set up a new config format

--- n2y/config.py
+++ n2y/config.py
@@ -1,10 +1,59 @@
 import json
 import logging
+import copy
+
+import yaml
 
 from n2y.utils import strip_hyphens
 
 
 logger = logging.getLogger(__name__)
+
+
+MASTER_DEFAULTS = {
+    "id_property": None,
+    "content_property": None,
+    "url_property": None,
+    "filename_property": None,
+    "plugins": [],
+    "filter": None,
+}
+
+
+def load_config(path):
+    try:
+        with open(path, "r") as config_file:
+            config = yaml.safe_load(config_file)
+    except yaml.YAMLError as exc:
+        logger.error("Error parsing the config file: %s", exc)
+        return None
+    except FileNotFoundError:
+        logger.error("The config file '%s' does not exist", path)
+        return None
+    merged_exports = merge_config(
+        config.get("exports", []),
+        MASTER_DEFAULTS,
+        config.get("export_defaults", {}),
+    )
+    config["exports"] = merged_exports
+    # TODO: update how validate_database_config works to handle the new format
+    # TODO: validate the database config using the updated validate_database_config
+    return config
+
+
+def merge_config(config_items, master_defaults, defaults):
+    """
+    For each config item, merge in both the user provided defaults and the
+    builtin master defaults for each key value pair."
+    """
+    merged_config_items = []
+    for config_item in config_items:
+        master_defaults_copy = copy.deepcopy(master_defaults)
+        defaults_copy = copy.deepcopy(defaults)
+        config_item_copy = copy.deepcopy(config_item)
+        merged_config_item = {**master_defaults_copy, **defaults_copy, **config_item_copy}
+        merged_config_items.append(merged_config_item)
+    return merged_config_items
 
 
 def database_config_json_to_dict(config_json):
--- tests/test_config.py
+++ tests/test_config.py
@@ -1,5 +1,60 @@
-from n2y.config import validate_database_config
+import yaml
+
+from n2y.config import validate_database_config, merge_config, load_config
 from n2y.notion_mocks import mock_id
+
+
+def test_load_config_basic(tmp_path):
+    # use a temporary file to test the config loading
+    config_path = tmp_path / "config.yaml"
+    export_id = mock_id()
+    with open(config_path, "w") as f:
+        f.write(yaml.dump({
+            "export_defaults": {
+                "id_property": "id",
+                "url_property": "url",
+            },
+            "exports": [
+                {
+                    "id": export_id,
+                    "pandoc_format": "gfm",
+                }
+            ]
+        }))
+    config = load_config(config_path)
+    merged_export = config["exports"][0]
+    assert merged_export["id"] == export_id
+    assert merged_export["id_property"] == "id"
+    assert merged_export["url_property"] == "url"
+    assert merged_export["pandoc_format"] == "gfm"
+
+
+def test_merge_config_no_defaults():
+    master_defaults = {"a": "1"}
+    defaults = {}
+    config_items = [
+        {"b": "1"},
+        {"b": "2", "a": "2"},
+    ]
+    assert merge_config(config_items, master_defaults, defaults) == [
+        {"a": "1", "b": "1"},
+        {"b": "2", "a": "2"},
+    ]
+
+
+def test_merge_config_defaults():
+    master_defaults = {"a": "1", "b": "1"}
+    defaults = {"a": "3"}
+    config_items = [
+        {},
+        {"a": "2"},
+        {"b": "2"},
+    ]
+    assert merge_config(config_items, master_defaults, defaults) == [
+        {"a": "3", "b": "1"},
+        {"a": "2", "b": "1"},
+        {"a": "3", "b": "2"},
+    ]
 
 
 def test_validate_database_config_empty():
