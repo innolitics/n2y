567fa1308ad55f1a1cc060436b6fd4728ec11a3a
Bimba Shrestha <bshrestha.msae@gmail.com>
2022-12-14 09:53:55 -0500

    Remove puppeter configuration

--- n2y/plugins/mermaid.py
+++ n2y/plugins/mermaid.py
@@ -19,13 +19,6 @@ mermaid_config = {
     }
 }
 
-puppeteer_config = {
-    "args": [
-        "--no-sandbox",
-        "--disable-setuid-sandbox",
-    ]
-}
-
 
 class MermaidFencedCodeBlock(FencedCodeBlock):
     """
@@ -51,16 +44,12 @@ class MermaidFencedCodeBlock(FencedCodeBlock):
         temp_config_mermaid_fd, temp_config_mermaid_filepath = tempfile.mkstemp(suffix=".json")
         os.write(temp_config_mermaid_fd, json.dumps(mermaid_config).encode("utf-8"))
         os.close(temp_config_mermaid_fd)
-        temp_config_puppeteer_fd, temp_config_puppeteer_filepath = tempfile.mkstemp(suffix=".json")
-        os.write(temp_config_puppeteer_fd, json.dumps(puppeteer_config).encode("utf-8"))
-        os.close(temp_config_puppeteer_fd)
         try:
             diagram_as_text = self.rich_text.to_plain_text()
             diagram_as_bytes = diagram_as_text.encode()
             subprocess.run([
                 'mmdc',
                 '--configFile', temp_config_mermaid_filepath,
-                '--puppeteerConfigFile', temp_config_puppeteer_filepath,
                 '-o', temp_filepath,
             ], capture_output=True, input=diagram_as_bytes, check=True)
             with open(temp_filepath, 'rb') as temp_file:
