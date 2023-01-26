80cfb410160f05bafac78c2d98b60532953bcde0
Sam'an Herman-Griffiths <shermangriffiths@innolitics.com>
2023-01-26 12:36:01 -0600

    add render tests

---
+++ tests/test_render.py
@@ -0,0 +1,52 @@
+from jinja2 import TemplateSyntaxError
+from pytest import raises
+
+from n2y.plugins.render import invert_dependencies, join_to
+from tests.utils import render_from_string
+
+
+def test_invert_dependencies_single():
+    objects = [
+        {'id': 'a', 'dependencies': ['r-1', 'r-2']}
+    ]
+    actual = invert_dependencies(objects, 'id', 'dependencies')
+    expected = [('r-1', {'a'}), ('r-2', {'a'})]
+    assert actual == expected
+
+
+def test_invert_dependencies_multiple():
+    objects = [
+        {'id': 'a', 'dependencies': ['r-1', 'r-2', 'r-3-2']},
+        {'id': 'b', 'dependencies': ['r-1', 'r-2', 'r-3-1']},
+    ]
+    actual = invert_dependencies(objects, 'id', 'dependencies')
+    expected = [
+        ('r-1', {'a', 'b'}),
+        ('r-2', {'a', 'b'}),
+        ('r-3-1', {'b'}),
+        ('r-3-2', {'a'}),
+    ]
+    assert actual == expected
+
+
+def test_join_to_basic():
+    foreign_keys = ['1', '3']
+    table = [
+        {'id': '1', 'data': 'a'},
+        {'id': '2', 'data': 'b'},
+    ]
+    assert join_to(foreign_keys, table) == [{'id': '1', 'data': 'a'}, None]
+    assert join_to(foreign_keys, table, 'data') == [None, None]
+
+
+def test_render_no_filtering():
+    input_string = "apple\nbanana\ncherry\n"
+    expected_result = input_string
+    actual_result = render_from_string(input_string)
+    assert actual_result == expected_result
+
+
+def test_undefined():
+    with raises(TemplateSyntaxError):
+        input_string = "{% huhwhat 'hotel', 'california' %}"
+        render_from_string(input_string)
