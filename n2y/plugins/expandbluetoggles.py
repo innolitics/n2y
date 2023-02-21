aebb0ee325522cb869ba47f067b8f42c9d348acb
Sam'an Herman-Griffiths <shermangriffiths@innolitics.com>
2023-02-21 15:20:43 -0600

    rename jinja toggle blocks

--- n2y/plugins/expandbluetoggles.py
+++ n2y/plugins/expandbluetoggles.py
@@ -9,7 +9,7 @@ plugin_data_key = "n2y.plugins.hiddenjinjatoggles"
 logger = logging.getLogger(__name__)
 
 
-class HiddenJinjaToggleBlock(ToggleBlock):
+class ExpandBlueToggleBlock(ToggleBlock):
     def __init__(self, client, notion_data, page, get_children=True):
         super().__init__(client, notion_data, page, get_children)
         if self.notion_type_data["color"] != "blue_background":
@@ -20,5 +20,5 @@ class HiddenJinjaToggleBlock(ToggleBlock):
 
 
 notion_classes = {
-    "blocks": {"toggle": HiddenJinjaToggleBlock},
+    "blocks": {"toggle": ExpandBlueToggleBlock},
 }
