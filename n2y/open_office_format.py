d94f6402bdc83f4dfdce08eb7d3b00bf3b511442
Matt Hayden <mhayden+github@innolitics.com>
2023-05-23 15:16:13 -0600

    Fix test which somehow accepted bytes

--- n2y/open_office_format.py
+++ n2y/open_office_format.py
@@ -1,9 +1,12 @@
 """
 Work with Open Office XML format documents.
 """
+import logging
 from textwrap import dedent
 from xml.etree import ElementTree
 import zipfile
+
+logger = logging.getLogger(__name__)
 
 ElementTree.register_namespace(
     "cp", "http://schemas.openxmlformats.org/package/2006/metadata/core-properties"
@@ -27,7 +30,7 @@ class OpenOfficeMetadata(zipfile.ZipFile):
         (title_element,) = root.findall(".//{*}title")
         return " ".join(title_element.itertext())
 
-    def set_title(self, text):
+    def set_title(self, text: str):
         info = self.getinfo("docProps/core.xml")
         root = ElementTree.fromstring(self.read(info))
         (title_element,) = root.findall(".//{*}title")
@@ -40,7 +43,7 @@ class OpenOfficeMetadata(zipfile.ZipFile):
         (template_element,) = root.findall(".//{*}Template")
         return " ".join(template_element.itertext())
 
-    def set_template(self, text):
+    def set_template(self, text: str):
         info = self.getinfo("docProps/app.xml")
         root = ElementTree.fromstring(self.read(info))
         (template_element,) = root.findall(".//{*}Template")
@@ -56,7 +59,7 @@ class OpenOfficeMetadata(zipfile.ZipFile):
         elements = root.findall(".//{*}property[@name]")
         return {e.attrib["name"]: " ".join(e.itertext()) for e in elements}
 
-    def _set_custom_properties(self, mapping):
+    def _set_custom_properties(self, mapping: dict[str, str]):
         mapping = dict(mapping)
         try:
             info = self.getinfo("docProps/custom.xml")
@@ -67,8 +70,15 @@ class OpenOfficeMetadata(zipfile.ZipFile):
             for element in root.findall(".//{*}property[@name]"):
                 if (name := element.attrib["name"]) in mapping:
                     # TODO: I suppose strictly, this would deserve its own namespace
-                    value = element.text = mapping.pop(name)
-                    element.attrib["type"] = type(value).__name__
+                    match (value := mapping.pop(name)):
+                        case str():
+                            element.text = value
+                            element.attrib["type"] = "str"
+                        case tuple() | list():
+                            element.text = ",".join(value)
+                            element.attrib["type"] = "str"
+                        case _:
+                            logger.error("Ignoring %r=%r", name, value)
         else:
             info = "docProps/custom.xml"
             root = ElementTree.fromstring(
@@ -84,12 +94,16 @@ class OpenOfficeMetadata(zipfile.ZipFile):
         parent = root
         for name, value in mapping.items():
             # TODO: I suppose strictly, this would deserve its own namespace
-            element = ElementTree.Element(
-                "{http://schemas.openxmlformats.org/officeDocument/2006/custom-properties}property",
-                attrib={"name": name, "type": type(value).__name__},
-            )
-            element.text = value
-            parent.append(element)
+            match value:
+                case str():
+                    element = ElementTree.Element(
+                        "{http://schemas.openxmlformats.org/officeDocument/2006/custom-properties}property",
+                        attrib={"name": name, "type": "str"},
+                    )
+                    element.text = value
+                    parent.append(element)
+                case _:
+                    logger.error("Ignoring %r=%r", name, value)
         self.writestr(info, ElementTree.tostring(root))
 
     def __getitem__(self, key):
