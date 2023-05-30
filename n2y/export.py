8c7c9562ae7d0e160d94fdfb3230e4d6106b3ea4
Sam'an Herman-Griffiths <shermangriffiths@innolitics.com>
2023-05-30 16:56:41 -0500

    make all custom errors children of N2YERROR, don't use export defaults when rendering Jinja, and makedirs in export_page so that single pages can be parented by directories

--- n2y/export.py
+++ n2y/export.py
@@ -8,7 +8,11 @@ import logging
 from pandoc.types import Table
 import yaml
 
-from n2y.utils import pandoc_format_to_file_extension, pandoc_write_or_log_errors, sanitize_filename
+from n2y.utils import (
+    pandoc_format_to_file_extension,
+    pandoc_write_or_log_errors,
+    sanitize_filename,
+)
 
 logger = logging.getLogger(__name__)
 
@@ -28,7 +32,8 @@ def _page_properties(
     if id_property in properties:
         logger.warning(
             'The id property "%s" is shadowing an existing '
-            'property with the same name', id_property,
+            "property with the same name",
+            id_property,
         )
     if id_property:
         properties[id_property] = page.notion_id
@@ -36,7 +41,8 @@ def _page_properties(
     if url_property in properties:
         logger.warning(
             'The url property "%s" is shadowing an existing '
-            'property with the same name', url_property,
+            "property with the same name",
+            url_property,
         )
     if url_property:
         properties[url_property] = page.notion_url
@@ -60,7 +66,9 @@ def export_page(
 ):
     pandoc_ast = page.to_pandoc()
 
-    if (number_empty_headers := _count_headerless_tables(pandoc_ast)) > 0:
+    if (number_empty_headers := _count_headerless_tables(pandoc_ast)) > 0 and (
+        "markdown" in pandoc_format or "gfm" in pandoc_format
+    ):
         logger.warning(
             "%d table(s) will present empty headers to maintain Markdown spec (%r)",
             number_empty_headers,
@@ -70,13 +78,19 @@ def export_page(
     page_content = pandoc_write_or_log_errors(pandoc_ast, pandoc_format, pandoc_options)
     if isinstance(page_content, str) and yaml_front_matter:
         page_properties = _page_properties(
-            page, pandoc_format, id_property, url_property, property_map,
+            page,
+            pandoc_format,
+            id_property,
+            url_property,
+            property_map,
         )
-        return '\n'.join([
-            '---',
-            yaml.dump(page_properties) + '---',
-            page_content,
-        ])
+        return "\n".join(
+            [
+                "---",
+                yaml.dump(page_properties) + "---",
+                page_content,
+            ]
+        )
     else:
         # if the result is a binary file, return it as is (since we can't add YAML metadata to it)
         return page_content
@@ -113,16 +127,21 @@ def database_to_yaml(
     if content_property in database.schema:
         logger.warning(
             'The content property "%s" is shadowing an existing '
-            'property with the same name', content_property,
+            "property with the same name",
+            content_property,
         )
     results = []
     for page in database.children_filtered(notion_filter, notion_sorts):
-        result = _page_properties(page, pandoc_format, id_property, url_property, property_map)
+        result = _page_properties(
+            page, pandoc_format, id_property, url_property, property_map
+        )
         if content_property:
             pandoc_ast = page.to_pandoc()
             if pandoc_ast:
                 result[content_property] = pandoc_write_or_log_errors(
-                    pandoc_ast, pandoc_format, pandoc_options,
+                    pandoc_ast,
+                    pandoc_format,
+                    pandoc_options,
                 )
             else:
                 result[content_property] = None
@@ -143,9 +162,8 @@ def database_to_files(
     url_property=None,
     property_map=None,
 ):
-    os.makedirs(directory, exist_ok=True)
     seen_file_names = set()
-    counts = {'unnamed': 0, 'duplicate': 0}
+    counts = {"unnamed": 0, "duplicate": 0}
     for page in database.children_filtered(notion_filter, notion_sorts):
         page_filename = _page_filename(page, pandoc_format, filename_template)
         if page_filename:
@@ -162,10 +180,12 @@ def database_to_files(
                 )
                 write_document(document, os.path.join(directory, page_filename))
             else:
-                logger.warning('Skipping page named "%s" since it has been used', page_filename)
-                counts['duplicate'] += 1
+                logger.warning(
+                    'Skipping page named "%s" since it has been used', page_filename
+                )
+                counts["duplicate"] += 1
         else:
-            counts['unnamed'] += 1
+            counts["unnamed"] += 1
     for key, count in counts.items():
         if count > 0:
             logger.info("%d %s page(s) skipped", count, key)
@@ -173,9 +193,10 @@ def database_to_files(
 
 def write_document(document, path):
     if isinstance(document, bytes):
-        file_mode = 'wb'
+        file_mode = "wb"
     else:
-        file_mode = 'w'
+        file_mode = "w"
+    os.makedirs(os.path.dirname(path), exist_ok=True)
     with open(path, file_mode) as f:
         f.write(document)
 
@@ -194,6 +215,7 @@ def _page_filename(page, pandoc_format, filename_template=None):
         except KeyError:
             logger.warning(
                 'Invalid filename property, "%s". Valid options are %s',
-                filename_template, ", ".join(page.properties.keys()),
+                filename_template,
+                ", ".join(page.properties.keys()),
             )
             return _page_filename(page, pandoc_format)
