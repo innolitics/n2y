c468516a7e26c7a13ddfa3531ffab380bbdcecbe
Sam'an Herman-Griffiths <100145229+sHermanGriffiths@users.noreply.github.com>
2024-02-06 10:08:28 -0600

    add info to log (#165)

--- n2y/logger.py
+++ n2y/logger.py
@@ -1,14 +1,18 @@
 import logging
+import os
+
+
+def filter_log(record: logging.LogRecord) -> logging.LogRecord:
+    """Filter log records."""
+    record.pathname = record.pathname.replace(os.getcwd() + "/", "")
+    return record
+
 
 FORMATTER = logging.Formatter(
-    "%(asctime)s - %(levelname)s - n2y.%(module)s: %(message)s"
+    "%(asctime)s - %(levelname)s (%(pathname)s::%(funcName)s::%(lineno)d): %(message)s"
 )
 HANDLER = logging.StreamHandler()
 HANDLER.setFormatter(FORMATTER)
+HANDLER.addFilter(filter_log)
 logging.basicConfig(level=logging.INFO, handlers=[HANDLER])
 logger: logging.Logger = logging.getLogger(__name__)
-
-
-def update_logger(new_logger: logging.Logger):
-    global logger
-    logger = new_logger
