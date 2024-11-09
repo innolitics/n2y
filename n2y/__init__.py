1d48612049bd4512bf3cfadc10d195241e47cb55
shermangriffiths <shermangriffiths@innolitics.com>
2024-11-09 11:48:37 -0600

    Improve `__init__` var names

--- n2y/__init__.py
+++ n2y/__init__.py
@@ -2,17 +2,17 @@ import logging
 import os
 
 
-def filter_log(record: logging.LogRecord) -> logging.LogRecord:
+def log_filter(record: logging.LogRecord) -> logging.LogRecord:
     record.pathname = record.pathname.replace(os.getcwd() + "/", "")
     return record
 
 
-FORMATTER = logging.Formatter(
+LOG_FORMATTER = logging.Formatter(
     "%(asctime)s - %(levelname)s (%(pathname)s::%(funcName)s::%(lineno)d): %(message)s"
 )
-HANDLER = logging.StreamHandler()
-HANDLER.setLevel(logging.INFO)
-HANDLER.setFormatter(FORMATTER)
-HANDLER.addFilter(filter_log)
+LOG_HANDLER = logging.StreamHandler()
+LOG_HANDLER.setLevel(logging.INFO)
+LOG_HANDLER.setFormatter(LOG_FORMATTER)
+LOG_HANDLER.addFilter(log_filter)
 LOG = logging.getLogger(__name__)
-LOG.addHandler(HANDLER)
+LOG.addHandler(LOG_HANDLER)
