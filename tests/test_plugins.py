da3588da3458af5ef444999c7317dc22787fbb76
Surinder <slohat@enestit.com>
2025-10-22 19:32:52 +0530

    no message

--- tests/test_plugins.py
+++ tests/test_plugins.py
@@ -10,7 +10,6 @@ from n2y.errors import PluginError, UseNextClass
 from n2y.notion_mocks import mock_paragraph_block
 from n2y.plugins.mermaid import MermaidFencedCodeBlock
 from n2y.plugins.rawcodeblocks import RawFencedCodeBlock
-from n2y.plugins.quoteblock import NotionQuoteBlock
 
 
 def test_load_plugin_invalid_notion_object():
@@ -111,15 +110,4 @@ def test_load_plugins_overrides_old_plugins():
     assert client.get_class_list("blocks", "code") == [
         FencedCodeBlock,
         RawFencedCodeBlock,
-    ]
-
-
-def test_load_quoteblock_plugin():
-    """Test loading the quoteblock plugin."""
-    from n2y.blocks import QuoteBlock
-    client = Client("")
-    client.load_plugins(["n2y.plugins.quoteblock"])
-    assert client.get_class_list("blocks", "quote") == [
-        QuoteBlock,
-        NotionQuoteBlock,
     ]
